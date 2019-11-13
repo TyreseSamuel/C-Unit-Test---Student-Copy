816f49de430db24d5550a0625d84b86f60a1fa8e
Tyrese Samuel <3tyrese@gmail.com>
2019-11-13 17:48:33 -0600

    Add project files.

---
+++ MathClasses/Matrix4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClasses
+{
+    public class Matrix4
+    {
+        public float m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16;
+
+        public Matrix4()
+        {
+            m11 = 1; m12 = 0; m13 = 0; m14 = 0;
+            m21 = 0; m22 = 1; m23 = 0; m24 = 0;
+            m31 = 0; m32= 0; m33= 1; m34= 0;
+            m41= 0; m42= 0; m43= 0; m44= 1;
+        }
+
+    }
+}
